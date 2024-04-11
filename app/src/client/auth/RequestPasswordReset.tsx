8e2174c9fc26f0da3fbe938c11111603d04f61a0
William Flanders <wflan@seas.upenn.edu>
2024-04-11 11:45:07 -0400

    pw request page

--- app/src/client/auth/RequestPasswordReset.tsx
+++ app/src/client/auth/RequestPasswordReset.tsx
@@ -1,10 +1,15 @@
 import { ForgotPasswordForm } from 'wasp/client/auth';
 import { AuthWrapper } from './authWrapper';
+import { AuthApperance } from '../../authAppearance';
+import Logo from '../static/BANNER-LOGO.svg';
 
 export function RequestPasswordReset() {
   return (
     <AuthWrapper>
-      <ForgotPasswordForm />
+      <ForgotPasswordForm 
+      appearance={AuthApperance}
+      logo={Logo}
+      />
     </AuthWrapper>
   );
 }
