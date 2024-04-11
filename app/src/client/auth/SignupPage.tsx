2d12e8f858017cb2b32eb9cf14e6a9a97413eb53
William Flanders <wflan@seas.upenn.edu>
2024-04-10 22:24:45 -0400

    sign in page form

--- app/src/client/auth/SignupPage.tsx
+++ app/src/client/auth/SignupPage.tsx
@@ -1,18 +1,24 @@
 import { SignupForm } from 'wasp/client/auth';
 import { Link } from 'react-router-dom';
 import { AuthWrapper } from './authWrapper';
+import { AuthApperance } from '../../authAppearance';
+import Logo from '../static/BANNER-LOGO.svg';
+
 
 export function Signup() {
   return (
     <AuthWrapper>
-      <SignupForm />
+      <SignupForm 
+      appearance={AuthApperance}
+      logo={Logo}
+      />
       <br />
       <span className='text-sm font-medium text-gray-900'>
-        I already have an account (
-        <Link to='/login' className='underline'>
-          go to login
+        Already have an account?&nbsp;
+        <Link to='/login' className='font-bold underline'>
+          Log in
         </Link>
-        ).
+        .
       </span>
       <br />
     </AuthWrapper>
