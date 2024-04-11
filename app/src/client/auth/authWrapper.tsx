411ce28f5c3c920ff573d33e671705099c5e8168
William Flanders <wflan@seas.upenn.edu>
2024-04-11 11:44:33 -0400

    fixed layout and added image for auth

--- app/src/client/auth/authWrapper.tsx
+++ app/src/client/auth/authWrapper.tsx
@@ -1,14 +1,19 @@
 import { ReactNode } from 'react';
+import banner from '../static/LOGIN-BANNER.svg';
 
 export function AuthWrapper({children} : {children: ReactNode }) {
   return (
-    <div className='flex justify-center min-h-screen flex-col pt-10 sm:px-6 lg:px-8'>
-      <div className='flex h-full sm:w-1/3 sm:max-w-1/3'>
-        <div className='bg-white py-8 px-4 sm:rounded-lg sm:px-10'>
-          <div className='-mt-8'>
-            { children }
-          </div>
+    <div className='grid grid-cols-3 min-h-screen'>
+      {/* Login form taking up 1/3 of the space */}
+      <div className='flex flex-col col-span-1 justify-center bg-white py-8 px-4 sm:rounded-lg sm:px-10'>
+        <div>
+          {children}
         </div>
+      </div>
+
+      {/* Image taking up 2/3 of the space */}
+      <div className="col-span-2 bg-gray-100 overflow-hidden"> {/* This ensures no overflow */}
+        <img className='w-full h-auto' src={banner} alt='Raffle Leader' style={{ objectFit: 'cover', height: '100vh' }} />
       </div>
     </div>
   );
