75ef6d8c1420578a4cf192c23fe7721d2fa64d30
William Flanders <wflan@seas.upenn.edu>
2024-04-27 12:30:21 -0400

    redo blog page

--- blog/src/utils/all.js
+++
@@ -1,8 +0,0 @@
-export const checkImageUrl = (image, url) => {
-    try {
-      new URL(image);
-      return image;
-    } catch (error) {
-      return new URL(image, url).toString();
-    }
-  };
