91fc6ec20bc565b6021d97ea2232973ff88bce8b
William Flanders <wflan@seas.upenn.edu>
2024-05-02 07:32:01 -0400

    Landing page text fix

--- app/src/client/landing-page/LandingPage.tsx
+++ app/src/client/landing-page/LandingPage.tsx
@@ -85,7 +85,7 @@ export default function LandingPage() {
           <Dialog.Panel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:bg-boxdark dark:text-white'>
             <div className='flex items-center justify-between'>
               <a href='/' className='-m-1.5 p-1.5'>
-                <span className='sr-only'>Your SaaS</span>
+                <span className='sr-only'>Raffle Leader</span>
                 <NavLogo />
               </a>
               <button
