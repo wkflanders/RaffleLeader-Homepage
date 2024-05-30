046cb7bdc069e2aceae6dbc123cc4b65586793dd
William Flanders <wflan@seas.upenn.edu>
2024-05-30 01:00:02 -0400

    FAQ component fix

--- app/src/client/components/FAQComponent.tsx
+++ app/src/client/components/FAQComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { faqs } from '../landing-page/contentSections';  // Ensure this path is correct
 
 function FAQComponent() {
