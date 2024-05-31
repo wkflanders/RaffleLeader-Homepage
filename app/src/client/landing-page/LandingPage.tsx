1c172f01431c0d53727d4aec5f984300ae6d3477
William Flanders <wflan@seas.upenn.edu>
2024-05-31 01:58:00 -0400

    remaining page conditionally loads after howitworkscomponent

--- app/src/client/landing-page/LandingPage.tsx
+++ app/src/client/landing-page/LandingPage.tsx
@@ -21,6 +21,7 @@ import StatsComponent from '../components/StatsComponent';
 
 export default function LandingPage() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [howItWorksCompleted, setHowItWorksCompleted] = useState(false);
 
   const { data: user, isLoading: isUserLoading } = useAuth();
 
@@ -191,145 +192,150 @@ export default function LandingPage() {
         </div>
 
         {/* Features section */}
-        <FeaturesComponent features={features} />
+        <FeaturesComponent features={features} onHowItWorksComplete={() => setHowItWorksCompleted(true)} />
 
-        {/* Stats section */}
-        <StatsComponent stats={stats}/>
+        {/* Conditionally render Stats section */}
+        {howItWorksCompleted && (
+          <>
+            {/* Stats section */}
+            <StatsComponent stats={stats}/>
 
-        {/* Pricing section */}
-        <div className='mx-auto max-w-4xl sm:px-6 lg:px-8 mt-60 grid grid-cols-1 gap-y-8 lg:gap-x-8 lg:grid-cols-2'>
-          {tiers.map((tier) => (
-            <div
-              key={tier.id}
-              className={cn(
-                'relative flex flex-col grow justify-between rounded-3xl ring-gray-900/10 dark:ring-gray-100/10 overflow-hidden p-8 xl:p-10  border-slate-700 border-2',
-                {
-                  'ring-2': tier.bestDeal,
-                  'ring-1 lg:my-6': !tier.bestDeal,
-                }
-              )}
-            >
-              {tier.bestDeal && (
-                <div className='absolute top-0 right-0 -z-10 w-full h-full transform-gpu blur-3xl' aria-hidden='true'>
-                  <div
-                    className='absolute w-full h-full bg-gradient-to-b from-raffleleader to-violet-400 opacity-15'
-                    style={{
-                      clipPath: 'circle(670% at 50% 50%)',
-                    }}
-                  />
+            {/* Pricing section */}
+            <div className='mx-auto max-w-4xl sm:px-6 lg:px-8 mt-60 grid grid-cols-1 gap-y-8 lg:gap-x-8 lg:grid-cols-2'>
+              {tiers.map((tier) => (
+                <div
+                  key={tier.id}
+                  className={cn(
+                    'relative flex flex-col grow justify-between rounded-3xl ring-gray-900/10 dark:ring-gray-100/10 overflow-hidden p-8 xl:p-10  border-slate-700 border-2',
+                    {
+                      'ring-2': tier.bestDeal,
+                      'ring-1 lg:my-6': !tier.bestDeal,
+                    }
+                  )}
+                >
+                  {tier.bestDeal && (
+                    <div className='absolute top-0 right-0 -z-10 w-full h-full transform-gpu blur-3xl' aria-hidden='true'>
+                      <div
+                        className='absolute w-full h-full bg-gradient-to-b from-raffleleader to-violet-400 opacity-15'
+                        style={{
+                          clipPath: 'circle(670% at 50% 50%)',
+                        }}
+                      />
+                    </div>
+                  )}
+                  <div className='mb-8'>
+                    <div className='flex items-center justify-between gap-x-4'>
+                      <h3 id={tier.id} className='text-center text-gray-900 text-2xl font-semibold leading-8 dark:text-white'>
+                        {tier.name}
+                      </h3>
+                    </div>
+                    <p className='font-bold mt-4 text-3xl text-gray-600 dark:text-white'><s>{tier.description}</s></p>
+                    <p className='mt-6 flex items-baseline gap-x-1 dark:text-white'>
+                      <span className='text-4xl font-bold tracking-tight text-gray-900 dark:text-white'>{tier.price}</span>
+                      <span className='text-sm font-semibold leading-6 text-gray-600 dark:text-white'>
+                        {tier.id !== TierIds.LIFETIME && '/yr'}
+                      </span>
+                    </p>
+                    <ul role='list' className='mt-8 space-y-3 text-sm leading-6 text-gray-600 dark:text-white'>
+                      {tier.features.map((feature) => (
+                        <li key={feature} className='flex gap-x-3'>
+                          <AiFillCheckCircle className='h-6 w-5 flex-none text-raffleleader' aria-hidden='true' />
+                          {feature}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  {!!user && user.hasPaid ? (
+                    <a
+                      href={'/pricing'}
+                      aria-describedby='manage-subscription'
+                      className={cn(
+                        'mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-raffleleader',
+                        {
+                          'bg-raffleleader text-white hover:text-white shadow-sm hover:bg-raffleleader': tier.bestDeal,
+                          'text-gray-600 ring-1 ring-inset ring-raffleleader hover:ring-raffleleader': !tier.bestDeal,
+                        }
+                      )}
+                    >
+                      Manage Subscription
+                    </a>
+                  ) : (
+                    <button
+                      onClick={handlePricingClick}
+                      aria-describedby={tier.id}
+                      className={cn(
+                        {
+                          'bg-raffleleader text-white hover:text-white shadow-sm hover:bg-raffleleader': tier.bestDeal,
+                          'text-gray-600  ring-1 ring-inset ring-raffleleader hover:text-white hover:bg-raffleleader': !tier.bestDeal,
+                        },
+                        'mt-8 block rounded-md py-2 px-3 text-center text-sm dark:text-white font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-raffleleader'
+                      )}
+                    >
+                      Get Now
+                    </button>
+                  )}
                 </div>
-              )}
-              <div className='mb-8'>
-                <div className='flex items-center justify-between gap-x-4'>
-                  <h3 id={tier.id} className='text-center text-gray-900 text-2xl font-semibold leading-8 dark:text-white'>
-                    {tier.name}
-                  </h3>
-                </div>
-                <p className='font-bold mt-4 text-3xl text-gray-600 dark:text-white'><s>{tier.description}</s></p>
-                <p className='mt-6 flex items-baseline gap-x-1 dark:text-white'>
-                  <span className='text-4xl font-bold tracking-tight text-gray-900 dark:text-white'>{tier.price}</span>
-                  <span className='text-sm font-semibold leading-6 text-gray-600 dark:text-white'>
-                    {tier.id !== TierIds.LIFETIME && '/yr'}
-                  </span>
-                </p>
-                <ul role='list' className='mt-8 space-y-3 text-sm leading-6 text-gray-600 dark:text-white'>
-                  {tier.features.map((feature) => (
-                    <li key={feature} className='flex gap-x-3'>
-                      <AiFillCheckCircle className='h-6 w-5 flex-none text-raffleleader' aria-hidden='true' />
-                      {feature}
+              ))}
+            </div>
+
+            {/* FAQ */}
+            <FAQComponent />
+
+            {/* Closing */}
+            <div className="bg-rl_wave bg-top bg-cover bg-no-repeat text-white text-center py-60 rounded-lg">
+              <h2 className="leading-loose text-xl md:text-4xl lg:text-7xl font-normal mb-4">Try <span className="font-semibold tracking-widest">Raffle Leader</span></h2>
+              <p className="leading-loose md:text-3xl lg:text-6xl mb-4">Risk Free For 14 Days</p>
+              <p className="leading-loose text-3xl mb-6 tracking-wider">And spend more time converting leads, not finding them.</p>
+              <div className='mt-10 flex items-center justify-center gap-x-6'>
+                <a
+                  href={'/pricing'}
+                  className='rounded-lg px-17 py-4 text-xl font-semibold bg-white text-raffleleader ring-1 ring-inset ring-gray-200 hover:ring-2 hover:ring-gray-200 hover:bg-gray-200 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:text-white'
+                >
+                  Get Started
+                </a>
+              </div>
+            </div>
+          </>
+        )}
+      </main>
+      {howItWorksCompleted && (
+        <div className='-mt-2 mx-auto dark:bg-boxdark-2'>
+          <footer
+            aria-labelledby='footer-heading'
+            className='relative bg-rldark py-24'
+          >
+            <h2 id='footer-heading' className='sr-only'>
+              Footer
+            </h2>
+            <div className='flex items-start justify-start ml-10 mt-10 gap-20'>
+              <div>
+                <h3 className='text-sm font-semibold leading-6 text-gray-300'>App</h3>
+                <ul role='list' className='mt-6 space-y-4'>
+                  {footerNavigation.app.map((item) => (
+                    <li key={item.name}>
+                      <a href={item.href} className='text-sm leading-6 text-white hover:text-gray-300'>
+                        {item.name}
+                      </a>
                     </li>
                   ))}
                 </ul>
               </div>
-              {!!user && user.hasPaid ? (
-                <a
-                  href={'/pricing'}
-                  aria-describedby='manage-subscription'
-                  className={cn(
-                    'mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-raffleleader',
-                    {
-                      'bg-raffleleader text-white hover:text-white shadow-sm hover:bg-raffleleader': tier.bestDeal,
-                      'text-gray-600 ring-1 ring-inset ring-raffleleader hover:ring-raffleleader': !tier.bestDeal,
-                    }
-                  )}
-                >
-                  Manage Subscription
-                </a>
-              ) : (
-                <button
-                  onClick={handlePricingClick}
-                  aria-describedby={tier.id}
-                  className={cn(
-                    {
-                      'bg-raffleleader text-white hover:text-white shadow-sm hover:bg-raffleleader': tier.bestDeal,
-                      'text-gray-600  ring-1 ring-inset ring-raffleleader hover:text-white hover:bg-raffleleader': !tier.bestDeal,
-                    },
-                    'mt-8 block rounded-md py-2 px-3 text-center text-sm dark:text-white font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-raffleleader'
-                  )}
-                >
-                  Get Now
-                </button>
-              )}
+              <div>
+                <h3 className='text-sm font-semibold leading-6 text-gray-300'>©2024 RAFFLE LEADER, INC.</h3>
+                <ul role='list' className='mt-6 space-y-4'>
+                  {footerNavigation.company.map((item) => (
+                    <li key={item.name}>
+                      <a href={item.href} className='text-sm leading-6 text-white hover:text-gray-300'>
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          ))}
+          </footer>
         </div>
-
-        {/* FAQ */}
-        <FAQComponent />
-
-        {/* Closing */}
-        <div className="bg-rl_wave bg-top bg-cover bg-no-repeat text-white text-center py-60 rounded-lg">
-          <h2 className="leading-loose text-xl md:text-4xl lg:text-7xl font-normal mb-4">Try <span className="font-semibold tracking-widest">Raffle Leader</span></h2>
-          <p className="leading-loose md:text-3xl lg:text-6xl mb-4">Risk Free For 14 Days</p>
-          <p className="leading-loose text-3xl mb-6 tracking-wider">And spend more time converting leads, not finding them.</p>
-          <div className='mt-10 flex items-center justify-center gap-x-6'>
-            <a
-              href={'/pricing'}
-              className='rounded-lg px-17 py-4 text-xl font-semibold bg-white text-raffleleader ring-1 ring-inset ring-gray-200 hover:ring-2 hover:ring-gray-200 hover:bg-gray-200 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:text-white'
-            >
-              Get Started
-            </a>
-          </div>
-        </div>
-      </main>
-
-      {/* Footer */}
-      <div className='-mt-2 mx-auto dark:bg-boxdark-2'>
-        <footer
-          aria-labelledby='footer-heading'
-          className='relative bg-rldark py-24'
-        >
-          <h2 id='footer-heading' className='sr-only'>
-            Footer
-          </h2>
-          <div className='flex items-start justify-start ml-10 mt-10 gap-20'>
-            <div>
-              <h3 className='text-sm font-semibold leading-6 text-gray-300'>App</h3>
-              <ul role='list' className='mt-6 space-y-4'>
-                {footerNavigation.app.map((item) => (
-                  <li key={item.name}>
-                    <a href={item.href} className='text-sm leading-6 text-white hover:text-gray-300'>
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <h3 className='text-sm font-semibold leading-6 text-gray-300'>©2024 RAFFLE LEADER, INC.</h3>
-              <ul role='list' className='mt-6 space-y-4'>
-                {footerNavigation.company.map((item) => (
-                  <li key={item.name}>
-                    <a href={item.href} className='text-sm leading-6 text-white hover:text-gray-300'>
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </footer>
-      </div>
+      )}
     </div>
   );
 }
